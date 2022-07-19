9510ec9f07a6cf765fc40181682c29d5db4415cc
Dhiego <dhiego.azevedo17@gmail.com>
2022-07-19 17:06:26 -0300

    commit initial

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,148 +1,85 @@
-<script setup lang="ts">
-import { useRouter } from "vue-router";
-import { useUserStore } from "@/stores/user";
-import ListCategory from "@/components/category/ListCategory.vue";
-
-const router = useRouter();
-const userStore = useUserStore();
-
-const logout = () => {
-  userStore.logout();
-  router.push({
-    name: "login",
-  });
-};
-</script>
-
 <template>
-  <q-layout view="lHh lpr lFf" style="height: 400px">
-    <q-header elevated class="bg-teal-6">
-      <div class="row">
-        <div class="col-3"></div>
-        <div class="col-6">
-          <q-toolbar>
-            <q-space></q-space>
-            <q-avatar>
-              <img
-                src="https://w7.pngwing.com/pngs/732/9/png-transparent-computer-icons-book-open-book-angle-rectangle-logo.png"
-              />
-            </q-avatar>
-
-            <q-toolbar-title>Store Book</q-toolbar-title>
-            <q-card
-              class="no-border no-shadow bg-transparent"
-              style="width: 300px"
-            >
-              <q-card-section class="q-pa-sm">
-                <q-input v-model="search" placeholder="Buscar livro">
-                  <template v-slot:append>
-                    <q-icon v-if="!search" name="search" />
-                    <q-icon
-                      v-else
-                      name="clear"
-                      class="cursor-pointer"
-                      @click="search = ''"
-                    />
-                  </template>
-                </q-input>
-              </q-card-section>
-            </q-card>
-            <q-space></q-space>
-          </q-toolbar>
-        </div>
-        <div class="col-3">
-          <q-toolbar>
-            <q-space></q-space>
-            <div
-              class="q-gutter-sm row items-center no-wrap"
-              style="margin-top: 15px"
-            >
-              <div v-if="userStore.loggedIn" class="q-mr-lg">
-                <q-btn
-                  round
-                  dense
-                  flat
-                  icon="fas fa-heart"
-                  style="color: #9d4182 !important"
-                  type="a"
-                  href="https://github.com/sponsors/pratik227"
-                  target="_blank"
-                >
-                </q-btn>
-                <q-btn round dense flat color="white" icon="notifications">
-                  <q-badge color="red" text-color="white" floating> 5 </q-badge>
-                  <q-menu>
-                    <q-list style="min-width: 100px">
-                      <messages></messages>
-                      <q-card class="text-center no-shadow no-border">
-                        <q-btn
-                          label="View All"
-                          style="max-width: 120px !important"
-                          flat
-                          dense
-                          class="text-indigo-8"
-                        >
-                        </q-btn>
-                      </q-card>
-                    </q-list>
-                  </q-menu>
-                </q-btn>
-                <!-- TODO-Deixar dinamico o carrinho -->
-                <q-btn
-                  round
-                  flat
-                  :to="{ name: 'shopping-cart', params: { id: 1 } }"
-                >
-                  <q-badge color="red" text-color="white" floating> 5 </q-badge>
-                  <q-icon name="shopping_cart" />
-                </q-btn>
-                <q-btn round flat>
-                  <q-avatar size="26px">
-                    <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-                  </q-avatar>
-                  <q-menu>
-                    <q-list style="min-width: 100px">
-                      <messages></messages>
-                      <q-card class="text-center no-shadow no-border">
-                        <q-btn
-                          label="Minhas Compras"
-                          style="max-width: 180px !important"
-                          flat
-                          dense
-                          class="text-indigo-8 q-pa-sm"
-                        >
-                        </q-btn>
-                      </q-card>
-                      <q-card class="text-center no-shadow no-border">
-                        <q-btn
-                          label="Sair"
-                          style="max-width: 180px !important"
-                          flat
-                          dense
-                          class="text-indigo-8 q-pa-sm"
-                          @click="logout()"
-                        >
-                        </q-btn>
-                      </q-card>
-                    </q-list>
-                  </q-menu>
-                </q-btn>
-              </div>
-              <div v-if="!userStore.loggedIn">
-                <q-btn :to="{ name: 'login' }">Entre</q-btn>
-                <q-btn :to="{ name: 'registration' }">Cadastre-se</q-btn>
-              </div>
-            </div>
-          </q-toolbar>
-        </div>
-      </div>
-
-      <ListCategory />
+  <q-layout
+    view="hHh Lpr lff"
+    container
+    style="height: 100vh"
+    class="shadow-2 rounded-borders"
+  >
+    <q-header elevated class="bg-black">
+      <q-toolbar>
+        <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+        <q-toolbar-title>SotreBook Admin</q-toolbar-title>
+      </q-toolbar>
     </q-header>
 
-    <q-page-container class="bg-grey-2">
-      <router-view />
+    <q-drawer
+      v-model="drawer"
+      show-if-above
+      :width="200"
+      :breakpoint="500"
+      bordered
+      class="bg-grey-3"
+    >
+      <q-scroll-area class="fit">
+        <q-list>
+          <template v-for="(menuItem, index) in menuList" :key="index">
+            <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
+              <q-item-section avatar>
+                <q-icon :name="menuItem.icon" />
+              </q-item-section>
+              <q-item-section>
+                {{ menuItem.label }}
+              </q-item-section>
+            </q-item>
+            <q-separator :key="'sep' + index" v-if="menuItem.separator" />
+          </template>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <q-page padding> <router-view /> </q-page>
     </q-page-container>
   </q-layout>
 </template>
-<script lang="ts"></script>
+
+<script>
+import { ref } from "vue";
+
+const menuList = [
+  {
+    icon: "shopping_cart",
+    label: "Vendas",
+    separator: true,
+  },
+  {
+    icon: "menu_book",
+    label: "Livros",
+    separator: false,
+  },
+  {
+    icon: "person_2",
+    label: "Autores",
+    separator: false,
+  },
+  {
+    icon: "category",
+    label: "Categorias",
+    separator: false,
+  },
+  {
+    icon: "maps_home_work",
+    label: "Editoras",
+    separator: false,
+  },
+];
+
+export default {
+  setup() {
+    return {
+      drawer: ref(false),
+      menuList,
+    };
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 import { useBookStore } from "@/stores/book";
-import CardBook from "../components/products/CardBook.vue";
 
 const booksStore = useBookStore();
 
@@ -11,16 +10,60 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="q-pa-lg bg-blue-grey-1">
-    <div class="row q-col-gutter-sm">
-      <div
-        class="col-md-3 col-lg-3 col-sm-12 col-xs-12 q-pa-lg"
-        v-for="book of booksStore.books"
-      >
-        <CardBook :data="book" />
+  <div class="q-pa-md" style="justify-content: center; display: flex">
+    <div class="shadow-2 rounded-borders container">
+      <div class="q-pa-md">
+        <q-card-section>
+          <div class="text-h6">Vendas</div>
+          <div class="text-subtitle3">Fiinalizadas</div>
+        </q-card-section>
+
+        <q-markup-table>
+          <thead>
+            <tr class="shadow-2 rounded-borders">
+              <th class="text-left">Capa</th>
+              <th class="text-center">Titulo</th>
+              <th class="text-center">Avaliação</th>
+              <th class="text-right">Valor</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="shadow-2 rounded-borders" style="height: 60px">
+              <td class="text-left">
+                <q-card>
+                  <q-img src="src/assets/imgs/books/O-Nome-do-Vento.jpg" />
+                </q-card>
+              </td>
+              <td class="text-center"><b>Titulo</b></td>
+              <td class="text-center">
+                <q-rating
+                  v-model="text"
+                  max="5"
+                  size="1.5em"
+                  color="yellow"
+                  icon="star_border"
+                  icon-selected="star"
+                  icon-half="star_half"
+                  readonly
+                  no-dimming
+                />
+              </td>
+              <td class="text-right">24</td>
+            </tr>
+          </tbody>
+        </q-markup-table>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.my-card {
+  width: 100%;
+}
+
+.container {
+  background-color: rgb(255, 255, 255);
+  width: 100%;
+}
+</style>
